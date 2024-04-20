1bc0cdda8acc33143c40c4671dedd4c7dac5235f
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2024-04-20 09:37:02 +0700

    initial summit

---
+++ CatalystTechSession/CatalystTechSession/CatalystTechSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalystTechSessionApp.swift
+//  CatalystTechSession
+//
+//  Created by Achmad Rijalu on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CatalystTechSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
